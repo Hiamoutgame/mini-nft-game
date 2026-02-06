35f26dc724e7add6c29c1a867cb4ae057c7001bb
Hiamoutgame <anhvietanh1123@gmail.com>
2026-02-06 19:20:44 +0700

    update test adoption

--- test/testAdoption.test.js
+++ test/testAdoption.test.js
@@ -2,34 +2,19 @@ const Adoption = artifacts.require("Adoption");
 
 contract("Adoption", (accounts) => {
   let adoption;
-  let expectedAdopter;
-  const petId = 8;
+  let adopter;
+  const expectedPetId = 8;
   before(async () => {
     adoption = await Adoption.deployed();
   });
-
   describe("adopting a pet and retrieving account addresses", async () => {
-    before("adopt a pet using accounts[0]", async () => {
-      await adoption.adopt(petId, { from: accounts[0] });
-      expectedAdopter = accounts[0];
+    before(async () => {
+      await adoption.adopt(expectedPetId, { from: accounts[0] });
+      adopter = accounts[0];
     });
-
-    it("can fetch the address of an owner by pet id", async () => {
-      const adopter = await adoption.adopters(petId);
-      assert.equal(
-        adopter,
-        expectedAdopter,
-        "The owner of the adopted pet should be the first account.",
-      );
-    });
-
-    it("can fetch the collection of all pet owners' addresses", async () => {
+    it("can fetch the address of an adopted pet's owner", async()=>{
       const adopters = await adoption.getAdopters();
-      assert.equal(
-        adopters[petId],
-        expectedAdopter,
-        "The owner of the adopted pet should be in the collection.",
-      );
-    });
+      assert.equal(adopters[expectedPetId], adopter,"the owner of the adopted pet should be recorded");
+    })
   });
 });
